d1d1b4040bd94dc35361c40b2d1671879c168d23
Reem Alsharabi <Reem.sh30@gmail.com>
2022-04-25 04:49:30 +0300

    commit

--- OOP/src/MovieEcosystem.java
+++ OOP/src/MovieEcosystem.java
@@ -1,54 +1,65 @@
 import java.awt.Image;
 import java.util.ArrayList;
 
-public class MovieEcosystem {
+public class MovieEcosystem
+{
 	private String birthPlace;
 	private ArrayList awards;
 	private String info;
 	private Image image;
-	
 
-	public MovieEcosystem(String b, String i) {
-		birthPlace= b;
+	public MovieEcosystem(String b, String i)
+	{
+		birthPlace = b;
 		awards = new ArrayList<String>();
 		info = i;
-		image= new Image();
+		image = new Image();
 	}
 
-	public String getBirthPlace() {
+	public String getBirthPlace()
+	{
 		return birthPlace;
 	}
 
-	public void setBirthPlace(String birthPlace) {
+	public void setBirthPlace(String birthPlace)
+	{
 		this.birthPlace = birthPlace;
 	}
 
-	public ArrayList getAwards() {
+	public ArrayList getAwards()
+	{
 		return awards;
 	}
-	
-	public String getInfo() {
+
+	public String getInfo()
+	{
 		return info;
 	}
 
-	public void setInfo(String info) {
+	public void setInfo(String info)
+	{
 		this.info = info;
 	}
 
-	public Image getImage() {
+	public Image getImage()
+	{
 		return image;
 	}
 
-	public void setImage(Image image) {
+	public void setImage(Image image)
+	{
 		this.image = image;
 	}
-	
-	public void addAward(String a) {
+
+	public void addAward(String a)
+	{
 		awards.add(a);
 	}
+
 	@Override
-	public String toString() {
-		return "MovieEcosystem [birthPlace=" + birthPlace + ", awards=" + awards + ", information=" + info + ", image=" + image
-				+ "]";
+	public String toString()
+	{
+		return "MovieEcosystem [birthPlace=" + birthPlace + ", awards=" + awards + ", information=" + info + ", image="
+				+ image + "]";
 	}
 }
