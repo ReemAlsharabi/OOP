801c764c77c8f7dff16f7478f1eb5e3df9e44fb8
Reem Alsharabi <Reem.sh30@gmail.com>
2022-04-26 01:12:06 +0300

    formatting

--- OOP/src/Actor.java
+++ OOP/src/Actor.java
@@ -1,23 +1,29 @@
 import java.util.ArrayList;
 import java.time.LocalDate;
 
-public class Actor extends MovieEcosystem{
-	private ArrayList movies;
-	
-	public Actor(String name, String email, String mobile, LocalDate bDate, String bPlace, String info) {
+public class Actor extends MovieEcosystem
+{
+	private ArrayList<Movie> movies;
+
+	public Actor(String name, String email, String mobile, LocalDate bDate, String bPlace, String info)
+	{
 		super(name, email, mobile, bDate, bPlace, info);
 		movies = new ArrayList<Movie>();
 	}
 
-	public ArrayList getMovies() {
+	public ArrayList<Movie> getMovies()
+	{
 		return movies;
 	}
-	public void addMovie(Movie movie) {
+
+	public void addMovie(Movie movie)
+	{
 		movies.add(movie);
 	}
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return super.toString() + "Actor [movies=" + movies + "]";
 	}
 }
--- OOP/src/Producer.java
+++ OOP/src/Producer.java
@@ -1,25 +1,30 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class Producer extends MovieEcosystem{
-	private ArrayList movies;
-	
-	public Producer(String name, String email, String mobile, LocalDate bDate, String bPlace, String info) {
+public class Producer extends MovieEcosystem
+{
+	private ArrayList<Movie> movies;
+
+	public Producer(String name, String email, String mobile, LocalDate bDate, String bPlace, String info)
+	{
 		super(name, email, mobile, bDate, bPlace, info);
 		movies = new ArrayList<Movie>();
 	}
 
-	public ArrayList getMovies() {
+	public ArrayList<Movie> getMovies()
+	{
 		return movies;
 	}
-	
-	public void addMovie(Movie movie) {
+
+	public void addMovie(Movie movie)
+	{
 		movies.add(movie);
 	}
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return super.toString() + "Producer [movies=" + movies + "]";
 	}
-	
+
 }
