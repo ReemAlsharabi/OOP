2f3c448fb9c50b9f540454d3b51a4fa6a1b48f0e
Reem Alsharabi <Reem.sh30@gmail.com>
2022-04-25 04:48:52 +0300

    Merge branch 'master' of https://github.com/ReemCodes/OOP.git

--- OOP/src/Person.java
+++ OOP/src/Person.java
@@ -1,80 +1,96 @@
 import java.util.*;
 import java.time.LocalDate;
-public class Person {
-	//data fields
-    protected String  name;
-    protected String  email;
-    protected String  mobile;
-    protected LocalDate birthDate;
-    
-    //constructors
-    public Person() {
-    	this("","","",null);
-    }
-    
-    public Person(String name, String email,String mobile, LocalDate birthDate){
-    	this.name = name;
-    	this.email = email;
-    	this.mobile = mobile;
-    	this.birthDate =birthDate;  	
-    }
 
-    //Getters and Setters
-	public String getName() {
+public class Person
+{
+	// data fields
+	protected String name;
+	protected String email;
+	protected String mobile;
+	protected LocalDate birthDate;
+
+	// constructors
+	public Person()
+	{
+		this("", "", "", null);
+	}
+
+	public Person(String name, String email, String mobile, LocalDate birthDate)
+	{
+		this.name = name;
+		this.email = email;
+		this.mobile = mobile;
+		this.birthDate = birthDate;
+	}
+
+	// Getters and Setters
+	public String getName()
+	{
 		return name;
 	}
 
-	public void setName(String name) throws Exception {
-	  boolean checkName = name.matches("[a-zA-Z]{2,}");  //only letters, will accept a name with at least 2 letters
-	  if(checkName == false){
-		  throw new Exception("Name is not valid");
-	  }
-	  else{
-	      this.name = name;}
+	public void setName(String name) throws Exception
+	{
+		boolean checkName = name.matches("[a-zA-Z]{2,}"); // only letters, will accept a name with at least 2 letters
+		if (checkName == false)
+		{
+			throw new Exception("Name is not valid");
+		} else
+		{
+			this.name = name;
+		}
 	}
 
-	public String getEmail() {
+	public String getEmail()
+	{
 		return email;
 	}
 
-	public void setEmail(String email) throws Exception {
-	  boolean checkEmail = email.matches("^[a-zA-Z0-9_+&-]+(?:\\.[a-zA-Z0-9_+&-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
-	  if(!checkEmail){		  
-		  throw new Exception("Email is not valid");
-	  }
-	  else
-		  this.email = email;
+	public void setEmail(String email) throws Exception
+	{
+		boolean checkEmail = email
+				.matches("^[a-zA-Z0-9_+&-]+(?:\\.[a-zA-Z0-9_+&-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+		if (!checkEmail)
+		{
+			throw new Exception("Email is not valid");
+		} else
+			this.email = email;
 	}
 
-	public String getMobile() {
+	public String getMobile()
+	{
 		return mobile;
 	}
 
-	public void setMobile(String mobile) throws Exception {
+	public void setMobile(String mobile) throws Exception
+	{
 		boolean checkMobile = mobile.matches("05[\\d]{8}");
-		if(!checkMobile){
+		if (!checkMobile)
+		{
 			throw new Exception("Mobile number is not valid");
-		}
-		else
-		    this.mobile = mobile;
+		} else
+			this.mobile = mobile;
 	}
-	
-    public LocalDate getBirthDate() {
+
+	public LocalDate getBirthDate()
+	{
 		return birthDate;
 	}
 
-	public void setBirthDate(LocalDate birthDate) {
+	public void setBirthDate(LocalDate birthDate)
+	{
 		boolean checkDate = (birthDate.toString()).matches("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$");// DD/MM/YYYY
-		if(!checkDate){
+		if (!checkDate)
+		{
 			System.out.println("Date is not valid");
-		}
-		else
+		} else
 			this.birthDate = birthDate;
-		
+
 	}
 
-	//methods
-	public String toString(){
-		return "Name: " + name + "\nEmail:" + email + "\nMobile number:" + mobile +"\nBirthDate: " + birthDate;
+	// methods
+	public String toString()
+	{
+		return "Name: " + name + "\nEmail:" + email + "\nMobile number:" + mobile + "\nBirthDate: " + birthDate;
 	}
 }
--- OOP/src/User.java
+++ OOP/src/User.java
@@ -1,32 +1,34 @@
 import java.time.LocalDate;
 import java.util.*;
-public class User extends Login {
-	//data fields 
-    private static int AccountNumber = 0;
-    private ArrayList <Reviews> review = new ArrayList<Reviews>();
-    
-    
-    //constructors
-    public User(){
-    	dateCreated = LocalDate.now();
-    	AccountNumber += 1;
-    }
-    
-    //Getters and setters
-   
-	public int getAccountNumber(){
+
+public class User extends Login
+{
+	// data fields
+	private static int AccountNumber = 0;
+	private ArrayList<Reviews> review = new ArrayList<Reviews>();
+
+	// constructors
+	public User()
+	{
+		dateCreated = LocalDate.now();
+		AccountNumber += 1;
+	}
+
+	// Getters and setters
+
+	public int getAccountNumber()
+	{
 		return AccountNumber;
 	}
 
-	
-	public void addReviews(User user, Movie movie, String comment, double ratingValue){
-		review.add(new Reviews(user,movie,comment,ratingValue));
+	public void addReviews(User user, Movie movie, String comment, double ratingValue)
+	{
+		review.add(new Reviews(user, movie, comment, ratingValue));
 	}
-	
-    
-    public String toString() {
-        return super.getUsername()+ "reviewing history: " + review  ;
-  }
 
+	public String toString()
+	{
+		return super.getUsername() + "reviewing history: " + review;
+	}
 
 }
